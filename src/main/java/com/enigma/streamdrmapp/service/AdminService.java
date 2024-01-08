e7dda10d08572fb97081f31f2e1136ccba0874ed
alvinrizal <alvinrizal911@gmail.com>
2024-01-08 19:09:22 +0700

    [Add] Controller

--- src/main/java/com/enigma/streamdrmapp/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.enigma.streamdrmapp.service;
-
-import com.enigma.streamdrmapp.entity.Admin;
-import com.enigma.streamdrmapp.response.ResponseAdmin;
-
-public interface AdminService {
-    ResponseAdmin createNewAdmin(Admin admin);
-}
